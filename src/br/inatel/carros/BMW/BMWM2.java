ba29310af122573bab4bcca5e07b5d95f5d7f079
Guilherme-O-Brito <gui_o_brito@yahoo.com.br>
2023-11-25 21:25:24 -0300

    Implementacao de setters e do configurador da BMW X6 e BMW M2

--- src/br/inatel/carros/BMW/BMWM2.java
+++ src/br/inatel/carros/BMW/BMWM2.java
@@ -1,5 +1,7 @@
 package br.inatel.carros.BMW;
 
+import java.util.Scanner;
+
 import br.inatel.Motor.Motor;
 import br.inatel.carros.Carro;
 import br.inatel.interfaces.Configurador;
@@ -14,12 +16,8 @@ public class BMWM2 extends Carro implements Configurador {
 
     // Metodos
     public BMWM2(String modelo, String corInterior, Motor motor, float velocidadeMaxima, String tracao, String cor, float preco, 
-                 String chassi, String cambio, int ano, String suspensao, String teto, String rodas, String aerofolio){
+                 String chassi, String cambio, int ano){
         super("BMW", modelo, corInterior, motor, velocidadeMaxima, tracao, cor, preco, chassi, cambio, ano);
-        this.suspensao = suspensao;
-        this.teto = teto;
-        this.rodas = rodas;
-        this.aerofolio = aerofolio;
     }
 
     @Override
@@ -34,6 +32,119 @@ public class BMWM2 extends Carro implements Configurador {
     @Override
     public void configurador() {
         
+        Scanner input = new Scanner(System.in);
+        System.out.println("Configurando BMW M2 " + modelo);
+        System.out.println("Selecione uma das opcoes de suspensao: ");
+        System.out.println("1 - Suspensao convencional                           : custo R$0");
+        System.out.println("2 - Suspensao coilover de corrida                    : Custo R$58.000");
+
+        switch(input.nextInt()) {
+            
+            case 1:
+                this.suspensao = "Suspensao convencional";
+                System.out.println("Suspensao convencional selecionado!");
+                break;
+
+            case 2:
+                this.suspensao = "Suspensao coilover de corrida";
+                this.preco += 58000;
+                System.out.println("Suspensao coilover de corrida selecionado!");
+                break;
+
+            default:
+                System.out.println("Opcao invalida!!! Selecionando opcao padrao: Suspensao convencional");
+                this.suspensao = "Suspensao convencional";
+
+        }
+
+        System.out.println("Selecione uma das opcoes de teto: ");
+        System.out.println("1 - Teto convencional             : custo R$0");
+        System.out.println("2 - Teto Solar                    : Custo R$8.000");
+
+        switch(input.nextInt()) {
+            
+            case 1:
+                this.teto = "Teto convencional";
+                System.out.println("Teto convencional selecionado!");
+                break;
+
+            case 2:
+                this.teto = "Teto Solar";
+                this.preco += 8000;
+                System.out.println("Teto Solar selecionado!");
+                break;
+
+            default:
+                System.out.println("Opcao invalida!!! Selecionando opcao padrao: Teto convencional");
+                this.teto = "Teto convencional";
+
+        }
+
+        System.out.println("Selecione uma das opcoes de teto: ");
+        System.out.println("1 - Rodas convencionais             : custo R$0");
+        System.out.println("2 - Rodas Esportivas BMW            : Custo R$5.500");
+
+        switch(input.nextInt()) {
+            
+            case 1:
+                this.rodas = "Rodas convencionais";
+                System.out.println("Rodas convencionais selecionado!");
+                break;
+
+            case 2:
+                this.rodas = "Rodas Esportivas BMW";
+                this.preco += 5500;
+                System.out.println("Rodas Esportivas BMW selecionado!");
+                break;
+
+            default:
+                System.out.println("Opcao invalida!!! Selecionando opcao padrao: Rodas convencionais");
+                this.rodas = "Rodas convencionais";
+
+        }
+
+        System.out.println("Selecione uma das opcoes de aerofolio: ");
+        System.out.println("1 - Aerofolio convencional                           : custo R$0");
+        System.out.println("2 - Aerofolio Esportivo BMW                          : Custo R$2.100");
+
+        switch(input.nextInt()) {
+            
+            case 1:
+                this.aerofolio = "Aerofolio convencional";
+                System.out.println("Aerofolio convencional selecionado!");
+                break;
+
+            case 2:
+                this.aerofolio = "Aerofolio Esportivo BMW";
+                this.preco += 2100;
+                System.out.println("Aerofolio Esportivo BMW selecionado!");
+                break;
+
+            default:
+                System.out.println("Opcao invalida!!! Selecionando opcao padrao: Aerofolio convencional");
+                this.aerofolio = "Aerofolio convencional";
+
+        }
+
+        input.close();
+
     }
     
+    // Setters
+    public void setSuspensao(String suspensao){
+        this.suspensao = suspensao;
+    }
+
+    public void setTeto(String teto){
+        this.teto = teto;
+    }
+
+    public void setRodas(String rodas){
+        this.rodas = rodas;
+    }
+
+    public void setAerofolio(String aerofolio){
+        this.aerofolio = aerofolio;
+    }
+
 }
--- src/br/inatel/carros/BMW/BMWX6.java
+++ src/br/inatel/carros/BMW/BMWX6.java
@@ -1,5 +1,7 @@
 package br.inatel.carros.BMW;
 
+import java.util.Scanner;
+
 import br.inatel.Motor.Motor;
 import br.inatel.carros.Carro;
 import br.inatel.interfaces.Configurador;
@@ -31,6 +33,92 @@ public class BMWX6 extends Carro implements Configurador {
     @Override
     public void configurador() {
         
+        Scanner input = new Scanner(System.in);
+        System.out.println("Configurando BMW X6 " + modelo);
+        System.out.println("Selecione uma das opcoes de teto: ");
+        System.out.println("1 - Teto convencional             : custo R$0");
+        System.out.println("2 - Teto Solar                    : Custo R$8.000");
+
+        switch(input.nextInt()) {
+            
+            case 1:
+                this.teto = "Teto convencional";
+                System.out.println("Teto convencional selecionado!");
+                break;
+
+            case 2:
+                this.teto = "Teto Solar";
+                this.preco += 8000;
+                System.out.println("Teto Solar selecionado!");
+                break;
+
+            default:
+                System.out.println("Opcao invalida!!! Selecionando opcao padrao: Teto convencional");
+                this.teto = "Teto convencional";
+
+        }
+
+        System.out.println("Selecione uma das opcoes de motorizacao: ");
+        System.out.println("1 - Motor a combustao convencional             : custo R$0");
+        System.out.println("2 - Motor hibrido eletrico/combustao           : Custo R$75.000");
+
+        switch(input.nextInt()) {
+            
+            case 1:
+                this.hibrido = false;
+                System.out.println("Motor a combustao convencional selecionado!");
+                break;
+
+            case 2:
+                this.hibrido = true;
+                this.preco += 75000;
+                System.out.println("Motor hibrido eletrico/combustao selecionado!");
+                break;
+
+            default:
+                System.out.println("Opcao invalida!!! Selecionando opcao padrao: Motor a combustao convencional");
+                this.hibrido = false;
+
+        }
+        
+        System.out.println("Selecione uma das opcoes de teto: ");
+        System.out.println("1 - Rodas convencionais             : custo R$0");
+        System.out.println("2 - Rodas Esportivas BMW            : Custo R$5.500");
+
+        switch(input.nextInt()) {
+            
+            case 1:
+                this.rodas = "Rodas convencionais";
+                System.out.println("Rodas convencionais selecionado!");
+                break;
+
+            case 2:
+                this.rodas = "Rodas Esportivas BMW";
+                this.preco += 5500;
+                System.out.println("Rodas Esportivas BMW selecionado!");
+                break;
+
+            default:
+                System.out.println("Opcao invalida!!! Selecionando opcao padrao: Rodas convencionais");
+                this.rodas = "Rodas convencionais";
+
+        }
+
+        input.close();
+
     }
     
+    // Setters
+    public void setTeto(String teto){
+        this.teto = teto;
+    }
+
+    public void setHibrido(boolean hibrido){
+        this.hibrido = hibrido;
+    }
+
+    public void setRodas(String rodas){
+        this.rodas = rodas;
+    }
+
 }
